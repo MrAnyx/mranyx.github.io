dede0d757d6073ab9aae564925ffc2c12279d01c
MrAnyx <44176707+MrAnyx@users.noreply.github.com>
2019-12-09 13:06:59 +0100

    test

---
+++ precache-manifest.7a8ee4f6f7c1d6599d70504ddcb71c67.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04c9b7af737bea64456b60a6b10af970",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ae4dddbaf3a4d5b274c",
+    "url": "/static/css/main.6fde0600.chunk.css"
+  },
+  {
+    "revision": "60351b1535a59d532d46",
+    "url": "/static/js/2.42f176ef.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.42f176ef.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8ae4dddbaf3a4d5b274c",
+    "url": "/static/js/main.24d8ff06.chunk.js"
+  },
+  {
+    "revision": "5a6bf8ec436daada3e38",
+    "url": "/static/js/runtime-main.a8363002.js"
+  },
+  {
+    "revision": "798e7d94a413e31149bb1c1adc84aef2",
+    "url": "/static/media/image.798e7d94.jpg"
+  },
+  {
+    "revision": "2de368502c14077f6c070626cc5f51a3",
+    "url": "/static/media/logo.2de36850.png"
+  }
+]);
